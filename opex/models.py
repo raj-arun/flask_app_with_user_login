d752671d8f7cafc0066ee711dc6166f2a35daed4
ARUN RAJ <s.arun.raj@gmail.com>
2019-03-02 22:11:00 -0500

    User Login and Logout Functionality added
    type: feat
    
    flask login_manager module is used to login and logout the user
    get_id function has been added to the User class overriding the standard function to use "user_id" instead of "id"

--- opex/models.py
+++ opex/models.py
@@ -1,7 +1,12 @@
 from datetime import datetime
-from opex import db
+from opex import db, login_manager
+from flask_login import UserMixin
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+	return User.query.get(int(user_id))	
+
+class User(db.Model, UserMixin):
 	user_id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(20), unique=True, nullable=False)
 	email = db.Column(db.String(30), unique=True, nullable=False)
@@ -11,6 +16,9 @@ class User(db.Model):
 
 	def __repr__(self):
 		return f"User('{self.username}', '{self.email}')"
+
+	def get_id(self):
+		return (self.user_id)
 
 class Post(db.Model):
 	post_id = db.Column(db.Integer, primary_key=True)
--- opex/routes.py
+++ opex/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from opex import app, db, bcrypt
 from opex.forms import RegistrationForm, LoginForm
 from opex.models import User, Post
+from flask_login import login_user, current_user, logout_user
 
 @app.route("/")
 @app.route("/home")
@@ -16,6 +17,9 @@ def about():
 
 @app.route("/register", methods=['GET','POST'])
 def register():
+	if current_user.is_authenticated:
+		return redirect(url_for('home')) 
+
 	form  = RegistrationForm()
 	if form.validate_on_submit():
 		hashed_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -29,11 +33,21 @@ def register():
 
 @app.route("/login", methods=['GET','POST'])
 def login():
+	if current_user.is_authenticated:
+		return redirect(url_for('home')) 	
 	form = LoginForm()
 	if form.validate_on_submit():
-		if form.email.data == 'araj@gmail.com' and form.password.data == 'welcome123': 
+		user = User.query.filter_by(email = form.email.data).first()
+		if user and bcrypt.check_password_hash(user.password,form.password.data):
+			login_user(user, remember=form.remember.data)
 			flash(f'Welcome {form.email.data}!','success')
 			return redirect(url_for('home'))
 		else:
 			flash('Login Unsuccessful!!', 'danger')     
 	return render_template('login.html', title='Login', form=form) 
+
+
+@app.route("/logout")
+def logout():
+	logout_user()
+	return redirect(url_for('home'))
