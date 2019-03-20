a1dd919c687ff3d9643071e8ce4b2524d6314941
ARUN RAJ <s.arun.raj@gmail.com>
2019-03-19 22:29:37 -0400

    Password Reset Option
    type: feat
    
    Added password reset token generation and verification methods to the User class

--- opex/models.py
+++ opex/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
-from opex import db, login_manager
+from opex import db, login_manager, app
 from flask_login import UserMixin
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -13,6 +14,19 @@ class User(db.Model, UserMixin):
 	image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
 	password = db.Column(db.String(60), nullable=False)
 	posts = db.relationship('Post', backref='author', lazy=True)
+
+	def get_reset_token(self, expires_in=1500):
+		s = Serializer(app.config['SECRET_KEY'],expires_in)
+		return s.dumps({'user_id':self.user_id}).decode('utf-8')
+
+	@staticmethod
+	def verify_reset_token(token):
+		s = Serializer(app.config['SECRET_KEY'])
+		try:
+			s.loads(token)['user_id']
+		except:
+			return None
+		return User.query.get(user_id)
 
 	def __repr__(self):
 		return f"User('{self.username}', '{self.email}')"
@@ -29,3 +43,7 @@ class Post(db.Model):
 
 	def __repr__(self):
 		return f"Post('{self.title}', '{self.date_posted}')"
+
+	def get_id(self):
+		return (self.post_id)
+		
